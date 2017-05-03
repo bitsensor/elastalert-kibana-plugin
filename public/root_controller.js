6668f66453bbd222862a9d13ecadc9d81cc3ba1c
Wesley Ashkinazy <wesley.ashkinazy@gmail.com>
2017-05-03 14:08:26 -0500

    ported es5 patch to our fork

--- public/root_controller.js
+++ public/root_controller.js
@@ -1,6 +1,6 @@
 import 'angular-aria';
 import 'angular-animate';
-import 'angular-material';
+import 'angular-material/angular-material.min.js';
 import 'angular-material/angular-material.css';
 import './services/api';
 import modules from 'ui/modules';
@@ -13,8 +13,4 @@ modules
     $mdThemingProvider.theme('default')
       .primaryPalette('red')
       .accentPalette('amber');
-
-    /*$mdIconProvider
-      .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
-      .defaultIconSet('img/icons/sets/core-icons.svg', 24);*/
   });
