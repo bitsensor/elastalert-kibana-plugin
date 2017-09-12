6ca907700fe0f04eb668c77e2857ad98f8daddf9
Ruben van Vreeland <ruben@bitsensor.io>
2017-09-12 16:11:13 +0200

    Only build latest

--- build_5.0-5.4_range.sh
+++ build_5.0-5.4_range.sh
@@ -5,6 +5,5 @@ mkdir artifact
 while read kibana_version; do
   cat package.json.template | sed "s/kibana-version/$kibana_version/g" > package.json
   npm run build --quiet
-  cp build/elastalert-$package_version.zip artifact/elastalert-$kibana_version-$package_version.zip
   cp build/elastalert-$package_version.zip artifact/elastalert-$kibana_version-latest.zip
 done < kibana-versions-5.0-5.4.txt
