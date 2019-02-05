9bbf0abac622d25e8fd03e1c42830f8d50d68ea9
Martijn Rondeel <martijn@rondeel.email>
2019-02-05 12:46:54 +0100

    Support Kibana 6.6.0
    - Kibana 6.6+ uses a newer HAPI version which comes with some breaking changes

--- server/routes/elastalert.js
+++ server/routes/elastalert.js
@@ -1,4 +1,4 @@
-export default function (server, options) {
+export default function(server, options) {
   const baseUri = `http${options.serverSsl ? 's' : ''}://${options.serverHost}:${options.serverPort}`;
 
   // Route every request to the ElastAlert API
@@ -7,8 +7,8 @@ export default function (server, options) {
     method: ['GET', 'POST', 'DELETE'],
     handler: {
       proxy: {
-        mapUri: function (request, callback) {
-          callback(null, `${baseUri}/${request.params.path || ''}`);
+        mapUri: request => {
+          return { uri: `${baseUri}/${request.params.path || ''}` };
         }
       }
     }
